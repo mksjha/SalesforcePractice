edd87a261592e892f3b7f0c1c3cbdbe3d19bdaf0
Mukesh Kumar Jha <mukeshkumar.jha@anz.com>
2020-03-06 10:31:43 +1100

    added date control

--- force-app/main/default/lwc/first/first.js
+++ force-app/main/default/lwc/first/first.js
@@ -3,13 +3,15 @@ import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 import TYPE_FIELD from '@salesforce/schema/Account.Type';
+import OWNER_FIELD from '@salesforce/schema/Account.OwnerId';
+import DATE_FIELD from '@salesforce/schema/Account.SLAExpirationDate__c';
 /**
  * Creates Account records.
  **/
 export default class AccountCreator extends LightningElement {
 
     accountObject = ACCOUNT_OBJECT;
-    myFields = [NAME_FIELD, WEBSITE_FIELD,TYPE_FIELD];
+    myFields = [NAME_FIELD, WEBSITE_FIELD,TYPE_FIELD,OWNER_FIELD,DATE_FIELD];
 
     handleAccountCreated(){
         // Run code when account is created.
