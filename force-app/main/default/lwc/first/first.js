9aabb6d4b5c4d51ad997afde990becd1b8c57c68
Mukesh Kumar Jha <mukeshkumar.jha@anz.com>
2020-03-05 16:26:53 +1100

    adding drop down

--- force-app/main/default/lwc/first/first.js
+++ force-app/main/default/lwc/first/first.js
@@ -2,14 +2,14 @@ import { LightningElement } from 'lwc';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
-
+import TYPE_FIELD from '@salesforce/schema/Account.Type';
 /**
  * Creates Account records.
  **/
 export default class AccountCreator extends LightningElement {
 
     accountObject = ACCOUNT_OBJECT;
-    myFields = [NAME_FIELD, WEBSITE_FIELD];
+    myFields = [NAME_FIELD, WEBSITE_FIELD,TYPE_FIELD];
 
     handleAccountCreated(){
         // Run code when account is created.
